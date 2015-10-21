86ec826a70e89942a81bc484ef408fc2643a1197
Stephan Renatus <stephan.renatus@aisec.fraunhofer.de>
2015-10-21 16:30:28 +0200

    Allow for renaming exposed methods
    This makes it easier to expose writers (`name=`):
    
    ```
    class A
      attr_accessor :name
      expose :name=, :name
    end
    ```
    
    such that in the DSL you can use `name "Elmo"` to set the name.

--- lib/cleanroom.rb
+++ lib/cleanroom.rb
@@ -75,12 +75,12 @@ module Cleanroom
     #
     # @param [Symbol] name
     #
-    def expose(name)
+    def expose(name, exposed_name = name)
       unless public_method_defined?(name)
         raise NameError, "undefined method `#{name}' for class `#{self.name}'"
       end
 
-      exposed_methods[name] = true
+      exposed_methods[exposed_name] = name
     end
 
     #
@@ -101,7 +101,7 @@ module Cleanroom
     # @return [Class]
     #
     def cleanroom
-      exposed = exposed_methods.keys
+      exposed = exposed_methods
       parent = self.name || 'Anonymous'
 
       Class.new(Object) do
@@ -125,8 +125,8 @@ module Cleanroom
           end
         end
 
-        exposed.each do |exposed_method|
-          define_method(exposed_method) do |*args, &block|
+        exposed.each do |exposed_name, exposed_method|
+          define_method(exposed_name) do |*args, &block|
             __instance__.public_send(exposed_method, *args, &block)
           end
         end
